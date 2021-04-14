d3ca985b91fa87e23b4109eef3d3bea6b2bf852d
Denis Bautin <denisbduis@gmail.com>
2021-04-15 04:01:51 +0500

    Animated Trajectory v0.9

--- TrajectoryTask/WpfApp/MainWindow.xaml.cs
+++ TrajectoryTask/WpfApp/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OxyPlot;
+using TrajectoryClasses;
 
 namespace WpfApp
 {
@@ -23,6 +28,73 @@ namespace WpfApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ThrowButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var trajectory = CreateTrajectoryCalculator()
+                .CalculateTrajectory(GetFloatFromBox(TimeBox));
+
+            UpdateLabels(trajectory);
+            AnimateTrajectory(trajectory);
+        }
+
+        private TrajectoryCalculator CreateTrajectoryCalculator()
+        {
+            return new TrajectoryCalculator(
+                new PointF(
+                    GetFloatFromBox(X0Box),
+                    GetFloatFromBox(Y0Box)),
+                GetFloatFromBox(SpeedBox),
+                GetFloatFromBox(AngleBox),
+                GetFloatFromBox(MassBox),
+                GetFloatFromBox(ResistanceBox));
+        }
+
+        private static float GetFloatFromBox(TextBox box)
+        {
+            return float.Parse(box.Text, CultureInfo.InvariantCulture);
+        }
+
+        private void UpdateLabels(Trajectory trajectory)
+        {
+            MaxHeightLabel.Content = trajectory.MaxHeight;
+            DistanceLabel.Content = trajectory.Distance;
+            FlightTimeLabel.Content = trajectory.FlightTimeInSeconds;
+        }
+
+        private void AnimateTrajectory(Trajectory trajectory)
+        {
+            LineSeries.ItemsSource = new List<DataPoint>(new[] { new DataPoint(0, 0) });
+            XAxis.Minimum = trajectory.MoveStates.First().Coords.X;
+            YAxis.Minimum = trajectory.MoveStates.First().Coords.Y;
+            XAxis.Maximum = trajectory.Distance + 4;
+            YAxis.Maximum = trajectory.MaxHeight + 4;
+            Dispatcher.Invoke(() => TrajectoryPlot.ActualModel.InvalidatePlot(true));
+
+            var sleepTime = (int)(GetFloatFromBox(TimeBox) * 1000);
+            Task.Run(() => StartTrajectoryAnimation(
+                trajectory,
+                sleepTime));
+        }
+
+        private void StartTrajectoryAnimation(Trajectory trajectory, int sleepTime)
+        {
+            var lst = LineSeries.ItemsSource as List<DataPoint>;
+            foreach (var state in trajectory.MoveStates.Skip(1))
+            {
+                var point = new DataPoint(state.Coords.X, state.Coords.Y);
+                lst.Add(point);
+                Dispatcher.Invoke(() => TrajectoryPlot.ActualModel.InvalidatePlot(true));
+
+                Thread.Sleep(sleepTime);
+            }
+        }
+
+        private void DrawTrajectory(Trajectory trajectory)
+        {
+            LineSeries.ItemsSource = trajectory.MoveStates
+                .Select(state => new DataPoint(state.Coords.X, state.Coords.Y));
         }
     }
 }
